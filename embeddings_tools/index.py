ef5b95a56b9c81857ae37684f9968f9034072c91
narugo1992 <narugo992@gmail.com>
2024-11-14 23:37:54 +0800

    dev(narugo): save that

--- embeddings_tools/index.py
+++ embeddings_tools/index.py
@@ -1,8 +1,10 @@
 import os
 
 import click
+import numpy as np
 from ditk import logging
 from embedding_reader.get_file_list import get_file_list
+from tqdm import tqdm
 
 from .utils import GLOBAL_CONTEXT_SETTINGS
 
@@ -18,7 +20,13 @@ def cli(input_dir: str):
 
     ids_dir = os.path.join(input_dir, 'ids')
     embs_dir = os.path.join(input_dir, 'embs')
-    print(get_file_list(ids_dir, file_format='npy'))
+
+    _, id_files = get_file_list(ids_dir, file_format='npy')
+    ids = []
+    for id_file in tqdm(id_files, desc='Concating ID files'):
+        ids.append(np.load(id_file))
+    ids = np.concatenate(ids)
+    logging.info(f'Shape of IDs data: {ids.shape!r}, dtype: {ids.dtype!r}')
 
 
 if __name__ == '__main__':
